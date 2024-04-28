1b33477195f66f73c81625e18c2f980e855d325a
vaishnavi185 <vishu032004@gmail.com>
2024-04-28 11:52:24 +0530

    keybord

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'; // Adjust path to OrbitControls
 
@@ -14,17 +13,30 @@ camera.position.z = 5;
 
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(1000, 500);
 
-const controls = new OrbitControls(camera,canvas);
-controls.enableDamping=true;
-controls.autoRotate=true;
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.enableKeys = true;
+controls.keys = {
+    LEFT: 37, //left arrow
+    UP: 38, // up arrow
+    RIGHT: 39, // right arrow
+    BOTTOM: 40 // down arrow
+};
+
+document.addEventListener('keydown', (event) => {
+    console.log('Key pressed:', event.key);
+});
 
 const rendererloop = () => {
-	controls.update();
-    renderer.render(scene, camera);
-    requestAnimationFrame(rendererloop);
+    controls.update(); // Update controls
+    renderer.render(scene, camera); // Render scene
+    requestAnimationFrame(rendererloop); // Request next frame
 };
 
 rendererloop();
+
+
 
